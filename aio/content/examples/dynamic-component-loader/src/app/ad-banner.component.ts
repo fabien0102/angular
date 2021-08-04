5964fb6a94f28a1ebf5287fd2cc8798f40b180af
Fabien BERNARD <fabien0102@hotmail.com>
2021-08-04 15:06:38 +0200

    Fix interval type
    `window.setInterval` return a `number`
    
    Note: TypeScript compiler can be confused between browser and nodejs `setInterval` type, using `window` prefix make explicit that we are using the browser API.

--- aio/content/examples/dynamic-component-loader/src/app/ad-banner.component.ts
+++ aio/content/examples/dynamic-component-loader/src/app/ad-banner.component.ts
@@ -21,7 +21,7 @@ export class AdBannerComponent implements OnInit, OnDestroy {
   @Input() ads: AdItem[] = [];
   currentAdIndex = -1;
   @ViewChild(AdDirective, {static: true}) adHost!: AdDirective;
-  interval: any;
+  interval: number;
 
   constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
 
@@ -31,7 +31,7 @@ export class AdBannerComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    clearInterval(this.interval);
+    window.clearInterval(this.interval);
   }
 
   loadComponent() {
@@ -48,7 +48,7 @@ export class AdBannerComponent implements OnInit, OnDestroy {
   }
 
   getAds() {
-    this.interval = setInterval(() => {
+    this.interval = window.setInterval(() => {
       this.loadComponent();
     }, 3000);
   }
